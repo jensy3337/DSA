bfcd7aa780d6f772fb7a08d2ab6f437425accd9f
Jensy <jensy3337@gmail.com>
2026-02-04 22:09:25 +0530

    Add script to find maximum value in a list

---
+++ maximum of list.py
@@ -0,0 +1,9 @@
+numbers = [45, 23, 67, 12, 89, 34, 56, 8, 90, 2]
+
+max_element = max(numbers)
+max_index = numbers.index(max_element)
+
+print("maximum value:")
+print(max_element)
+print("index of maximum value:")
+print(max_index)
