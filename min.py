8a8f34ef9d4f4e12122507667e54fd3cd8633c5c
Jensy <jensy3337@gmail.com>
2026-02-04 22:07:18 +0530

    Create min.py

---
+++ min.py
@@ -0,0 +1,9 @@
+numbers = [45, 23, 67, 12, 89, 34, 56, 8, 90, 2]
+
+min_element = min(numbers)
+min_index = numbers.index(min_element)
+
+print("minimum value:")
+print(min_element)
+print("index of minimum value:")
+print(min_index)
